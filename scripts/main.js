e622c4fe1e148c159c99032ef45874d726bb11d4
JensenAY <JensenAY6@gmail.com>
2025-02-02 18:26:57 +0200

    New Features
    Made stuff work better and add some responsiveness. Working on the text keeping an active style when pressed while removing the other styles of the other text using JS, Currently working on it still

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,38 @@ button.addEventListener("click", () => {
     screen.style.display = "none";
   }, 1000);
 });
+
+// Active Button on Click
+
+const btnHome = document.getElementById("home");
+const btnProjects = document.getElementById("projects");
+const btnExperience = document.getElementById("experience");
+const btnContacts = document.getElementById("contact");
+
+btnHome.addEventListener("onClick", () => {
+  btnHome.classList.add("btnActive");
+  btnProjects.classList.remove("btnActive");
+  btnExperience.classList.remove("btnActive");
+  btnContacts.classList.remove("btnActive");
+});
+
+btnProjects.addEventListener("onClick", () => {
+  btnHome.classList.remove("btnActive");
+  btnProjects.classList.add("btnActive");
+  btnExperience.classList.remove("btnActive");
+  btnContacts.classList.remove("btnActive");
+});
+
+btnExperience.addEventListener("onClick", () => {
+  btnHome.classList.remove("btnActive");
+  btnProjects.classList.remove("btnActive");
+  btnExperience.classList.add("btnActive");
+  btnContacts.classList.remove("btnActive");
+});
+
+btnContacts.addEventListener("onClick", () => {
+  btnHome.classList.remove("btnActive");
+  btnProjects.classList.remove("btnActive");
+  btnExperience.classList.remove("btnActive");
+  btnContacts.classList.add("btnActive");
+});
