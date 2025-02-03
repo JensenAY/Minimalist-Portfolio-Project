08e72b4b63b69c7be70c6a9c0598def2be6c6240
JensenAY <JensenAY6@gmail.com>
2025-02-03 16:42:18 +0200

    Major Update
    Fixed the buttons not staying active, Added content for the buttons. QOL updates

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const button = document.getElementById("view-portfolio");
 const screen = document.getElementById("loading-screen");
 
 button.addEventListener("click", () => {
+  btnHome.classList.add("btnActive");
   screen.style.animation = "remove 1s ease forwards";
   setTimeout(() => {
     screen.style.display = "none";
@@ -15,30 +16,55 @@ const btnProjects = document.getElementById("projects");
 const btnExperience = document.getElementById("experience");
 const btnContacts = document.getElementById("contact");
 
-btnHome.addEventListener("onClick", () => {
+const homeText = document.getElementById("home-button-text");
+const projectsText = document.getElementById("project-list");
+const experienceText = document.getElementById("experience-text");
+const contactsText = document.getElementById("contact-me");
+
+btnHome.addEventListener("click", () => {
   btnHome.classList.add("btnActive");
   btnProjects.classList.remove("btnActive");
   btnExperience.classList.remove("btnActive");
   btnContacts.classList.remove("btnActive");
+
+  homeText.style.display = "block";
+  projectsText.style.display = "none";
+  experienceText.style.display = "none";
+  contactsText.style.display = "none";
 });
 
-btnProjects.addEventListener("onClick", () => {
+btnProjects.addEventListener("click", () => {
   btnHome.classList.remove("btnActive");
   btnProjects.classList.add("btnActive");
   btnExperience.classList.remove("btnActive");
   btnContacts.classList.remove("btnActive");
+
+  homeText.style.display = "none";
+  projectsText.style.display = "flex";
+  experienceText.style.display = "none";
+  contactsText.style.display = "none";
 });
 
-btnExperience.addEventListener("onClick", () => {
+btnExperience.addEventListener("click", () => {
   btnHome.classList.remove("btnActive");
   btnProjects.classList.remove("btnActive");
   btnExperience.classList.add("btnActive");
   btnContacts.classList.remove("btnActive");
+
+  homeText.style.display = "none";
+  projectsText.style.display = "none";
+  experienceText.style.display = "flex";
+  contactsText.style.display = "none";
 });
 
-btnContacts.addEventListener("onClick", () => {
+btnContacts.addEventListener("click", () => {
   btnHome.classList.remove("btnActive");
   btnProjects.classList.remove("btnActive");
   btnExperience.classList.remove("btnActive");
   btnContacts.classList.add("btnActive");
+
+  homeText.style.display = "none";
+  projectsText.style.display = "none";
+  experienceText.style.display = "none";
+  contactsText.style.display = "flex";
 });
